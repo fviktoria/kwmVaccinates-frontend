d50b27a0e3f7061ffbde149bd6360ee971d4503f
Viktoria <hello@viktoriamf.com>
2021-04-17 14:30:28 +0200

    add classes

---
+++ src/app/shared/location.ts
@@ -0,0 +1,10 @@
+export class Location {
+  constructor(
+    public id: number,
+    public name: string,
+    public street: string,
+    public houseNo: string,
+    public city: string,
+    public zipCode: string,
+  ) {}
+}
---
+++ src/app/shared/vaccination.ts
@@ -0,0 +1,3 @@
+export class Vaccination {
+  constructor(public id: number, public maxPatients: number, public date: Date, public from: Date, public to: Date) {}
+}
