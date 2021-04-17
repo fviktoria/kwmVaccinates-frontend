e9f66eaf52ecbb8cf7d54c8de22766e70fe3b8ad
Viktoria <hello@viktoriamf.com>
2021-04-17 18:10:14 +0200

    list locations and vaccination dates in admin area

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationService } from '../shared/location.service';
+import Location from '../shared/location';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  constructor(private ls: LocationService) {}
+  locations: Location[];
 
   ngOnInit(): void {
+    this.ls.getAll().subscribe((res) => (this.locations = res));
   }
-
 }
--- src/app/vaccination-list-admin/vaccination-list-admin.component.ts
+++ src/app/vaccination-list-admin/vaccination-list-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import Location from '../shared/location';
+import { LocationService } from '../shared/location.service';
 import { Vaccination } from '../shared/vaccination';
 import { VaccinationService } from '../shared/vaccination.service';
 
@@ -13,10 +14,11 @@ export class VaccinationListAdminComponent implements OnInit {
   vaccinations: Vaccination[];
   location: Location;
 
-  constructor(private vs: VaccinationService, private route: ActivatedRoute) {}
+  constructor(private vs: VaccinationService, private ls: LocationService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     const params = this.route.snapshot.params;
     this.vs.getByLocation(params['locationId']).subscribe((res) => (this.vaccinations = res));
+    this.ls.getSingle(params['locationId']).subscribe((res) => (this.location = res));
   }
 }
