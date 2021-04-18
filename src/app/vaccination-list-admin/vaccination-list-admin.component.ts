3934ae0ddfcf0ea25d03902658df0b5db8434279
Viktoria <hello@viktoriamf.com>
2021-04-18 12:17:53 +0200

    implement deletion of vaccination date

--- src/app/vaccination-list-admin/vaccination-list-admin.component.ts
+++ src/app/vaccination-list-admin/vaccination-list-admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import Location from '../shared/location';
 import { LocationService } from '../shared/location.service';
 import { Vaccination } from '../shared/vaccination';
@@ -14,11 +14,24 @@ export class VaccinationListAdminComponent implements OnInit {
   vaccinations: Vaccination[];
   location: Location;
 
-  constructor(private vs: VaccinationService, private ls: LocationService, private route: ActivatedRoute) {}
+  constructor(
+    private vs: VaccinationService,
+    private ls: LocationService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     const params = this.route.snapshot.params;
     this.vs.getByLocation(params['locationId']).subscribe((res) => (this.vaccinations = res));
     this.ls.getSingle(params['locationId']).subscribe((res) => (this.location = res));
+  }
+
+  removeVaccination(id) {
+    if (confirm('Confirm deletion?')) {
+      this.vs.delete(id).subscribe((res) => {
+        this.vaccinations.splice(this.vaccinations.indexOf(this.vaccinations.find((item) => item.id == id)), 1);
+      });
+    }
   }
 }
