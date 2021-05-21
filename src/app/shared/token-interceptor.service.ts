945697c17f6e4741373aeaa151032f58f9986af1
Viktoria <hello@viktoriamf.com>
2021-05-21 16:54:59 +0200

    token interceptor

--- src/app/shared/token-interceptor.service.ts
+++ src/app/shared/token-interceptor.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 @Injectable()
 export class TokenInterceptorService implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
